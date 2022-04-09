92f63a5463d836e1a805ca58372a8d7b0bac5389
programer1983 <programer972@gmail.com>
2022-04-09 11:49:34 +0300

    commit-3;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
+import {useState} from "react"
+import FComponent from "./components/FComponent"
+import CComponent from "./components/CComponent"
 import './App.css';
 
-function App() {
+function App() { 
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => {
+    return setCounter(counter + 1)
+  }
+
+  const decrement = () => {
+    return setCounter(counter - 1)
+  }
+
   return (
-    <div className="App">
-      
+    <div className="App border">
+      <h1>App Content</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <FComponent />
+      <hr></hr>
+      <CComponent />
     </div>
   );
 }
